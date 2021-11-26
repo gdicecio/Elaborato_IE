cd962ece89a62ec231961011402f04207c5af0b3
gdicecio <giuseppefrancesco98@gmail.com>
2021-11-26 13:36:45 +0100

    capacity_test_matlab

---
+++ Homework/Capacity_Test/Ubuntu_WebServer/capacity_test.m
@@ -0,0 +1,45 @@
+clc, close all, clear all;
+
+%% Data
+workloads = [100 500 800 1000 2000 3000 4000 5000 6000 7000 8000 9000];
+througputs = zeros(1, length(workloads));
+resp_times = zeros(1, length(workloads));
+k = 1; %Indice di riferimento per i due vettori
+
+%% Elaborazione
+for i = workloads
+    mean_resp_t = zeros(1,3);
+    thr = zeros(1,3);
+    for j = 1:3
+        path = strcat(num2str(i, '%d'),'\dati',num2str(j,'%d'),'.csv');
+
+        %Data from Jmeter output file (csv format)
+        simple_data = readmatrix(path);
+        
+        %Calculating the number of requests
+        [N, M] = size(simple_data);
+        num_req = N; %Number of requests
+
+        %Throughput = number_of_requests_completed / time_window_of_the_experiment
+        t_wind_mills = simple_data(num_req,1) - simple_data(1,1); %Time window (milliseconds)
+        t_wind_sec = t_wind_mills/1000; %Time window (seconds)
+        thr(j) = num_req/t_wind_sec; %Throughput
+
+        %Average response time
+        elap_times = simple_data(:,2);
+        mean_resp_t(j) = mean(elap_times);
+       % median_resp_t = median(elap_times);
+    end
+    
+    througputs(k) = mean(thr);
+    resp_times(k) = mean(mean_resp_t);
+    k = k+1; 
+end
+
+%% Plot dei risultati
+plot(workloads, througputs);
+figure;
+plot(workloads, resp_times);
+grid;
+figure;
+plot(workloads, througputs./resp_times);
