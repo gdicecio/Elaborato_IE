0b3a2644c0f174587e604e535b827157ea303b78
gdicecio <giuseppefrancesco98@gmail.com>
2021-12-09 20:38:39 +0100

    inizio fase 1

--- Homework/WebServer_Characterization/Ubuntu_21 New/NoParametric.m
+++ Homework/WebServer_Characterization/Ubuntu_21 New/NoParametric.m
@@ -1,8 +1,8 @@
 function [p_wilc,h_wilc] = NoParametric(real,synthetic,N)
-p_wilc = zeros(1,N-1);
-h_wilc = zeros(1,N-1);
-for i = 1:N-1
-    [p_wilc(i),h_wilc(i)] = ranksum(synthetic(:,i), real(:,i));
-end
+    p_wilc = zeros(1,N-1);
+    h_wilc = zeros(1,N-1);
+    for i = 1:N-1
+        [p_wilc(i),h_wilc(i)] = ranksum(synthetic(:,i), real(:,i));
+    end
 end
 
