c9c815831f6531ec0c8b3965796bf817953b0f8a
gdicecio <giuseppefrancesco98@gmail.com>
2021-12-15 16:12:00 +0100

    finish

--- Homework/Design_of_Experiment/data/response_time.m
+++ Homework/Design_of_Experiment/data/response_time.m
@@ -22,3 +22,52 @@ for i=intensity
     factor_page = 1;
 end
 
+%% Analysis
+a = length(page_type);
+b = length(intensity);
+r = N;
+%Parametri del modello
+mu = 0;             %Media totale
+alpha = zeros(1, a);%Effetto di PAGE TYPE
+beta = zeros(1, b); %Effetto di INTENSITY
+gamma = zeros(b, a);%Effetto dell'interazione
+e = zeros(b, a, r); %Errore
+
+%Calcoli
+mu = 1/(a*b*r) * sum(resp_time,'all');
+for j=1:a
+    alpha(j) = sum(resp_time(:,j,:), 'all')/(r*b) - mu;
+end
+for i=1:b
+    beta(i) = sum(resp_time(i,:,:), 'all')/(r*a) - mu;
+end
+for i=1:b
+    for j=1:a
+     gamma(i,j) = sum(resp_time(i,j,:),'all')/r - mu - alpha(j) - beta(i);
+    end
+end
+for i=1:b
+    for j=1:a
+        for k=1:r
+            e(i,j,k) = resp_time(i,j,k) - sum(resp_time(i,j,:),'all')/r;
+        end
+    end
+end
+
+%% Parametri caratteristici
+SSY = sum(resp_time(:,:,:).^2, 'all');
+SS0 = a*b*r*mu*mu;
+SSA = b*r*sum(alpha.^2);
+SSB = a*r*sum(beta.^2);
+SSAB = r*sum(gamma.^2, 'all');
+SSE = sum(e.^2, 'all');
+SST = SSY - SS0;
+IMPORTANZA_PAGE_TYPE = SSA/SST;
+IMPORTANZA_INTENSITY = SSB/SST;
+IMPORTANZA_INTERACTION = SSAB/SST;
+IMPORTANZA_ERRORE = SSE/SST;
+
+MSA = SSA/(a-1);
+MSB = SSB/(b-1);
+MSAB = SSAB/((a-1)*(b-1));
+MSE = SSE/(a*b*(r-1));
