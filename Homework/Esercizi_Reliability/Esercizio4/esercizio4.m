5dd92b87447576e468dbaf5c5b911d97e5407b68
EmMe <emmamelluso@libero.it>
2022-01-03 10:04:30 +0100

    es4 fine

--- Homework/Esercizi_Reliability/Esercizio4/esercizio4.m
+++ Homework/Esercizi_Reliability/Esercizio4/esercizio4.m
@@ -1,5 +1,5 @@
 clc; close all; clear all;
-t = linspace(0,5000);
+t = linspace(0,9000);
 
 MTTF_A = 1000;
 MTTF_B = 9000;
@@ -21,7 +21,9 @@ plot(t,R_11);
 title('Confronto 1');
 hold on;
 plot(t,R_12);
-
+xlabel('Mission Time');
+ylabel('Reliability');
+grid;
 %% CONFRONTO 2
 R_21 = R_A.*(1-(1-R_A).*(1-R_B));
 R_22 = R_A;
@@ -30,16 +32,23 @@ plot(t,R_21);
 title('Confronto 2');
 hold on;
 plot(t,R_22);
+xlabel('Mission Time');
+ylabel('Reliability');
+grid;
 
 %% CONFRONTO 3
 R_31 = R_21.*R_B;
 R_32 = R_A.*R_B;
 figure();
-plot(t,R_31);
+g1 = plot(t,R_31);
 title('Confronto 3');
 hold on;
-plot(t,R_32);
-
+g2 = plot(t,R_32);
+set(g1,'LineWidth',1.5);
+set(g2,'LineWidth',1.5);
+xlabel('Mission Time');
+ylabel('Reliability');
+grid;
 %% CONFRONTO 4
 R_41 = 1-((1-R_A).*(1-R_A.*R_B));
 R_42 = R_A;
@@ -48,7 +57,9 @@ plot(t,R_41);
 title('Confronto 4');
 hold on;
 plot(t,R_42);
-
+xlabel('Mission Time');
+ylabel('Reliability');
+grid;
 
 
 
